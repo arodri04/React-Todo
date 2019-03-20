a1bfb32ebbb3f4522dfd5da35ed02b5c53dd02ad
arodri04 <augustine.rodriguez55@gmail.com>
2019-03-20 17:56:56 -0600

    this really made me wanna cry

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React from "react";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import Todo from "./components/TodoComponents/Todo";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: "Bake Cookies",
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      task: ""
+    };
+  }
+
+  Change = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  update = event => {
+    event.preventDefault();
+
+    let todosCopy = this.state.todos.slice();
+    todosCopy.push({ task: this.state.task, id: Date.now(), completed: false });
+    this.setState({ todos: todosCopy, todoInput: "" });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="main">
+        <div>
+          {this.state.todos.map(todo => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </div>
+        <TodoForm
+          task={this.state.task}
+          Change={this.Change}
+          update={this.update}
+        />
       </div>
     );
   }
