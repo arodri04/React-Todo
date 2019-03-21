6b1f832ea868325b90e78a5c6731293d7677d7ed
arodri04 <augustine.rodriguez55@gmail.com>
2019-03-21 15:07:55 -0600

    Im gonna go cry now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import TodoForm from "./components/TodoComponents/TodoForm";
-import Todo from "./components/TodoComponents/Todo";
+import "./components/TodoComponents/Todo.css";
 
 class App extends React.Component {
   constructor() {
@@ -19,37 +18,52 @@ class App extends React.Component {
           completed: false
         }
       ],
-      task: ""
+      todoInput: ""
     };
   }
 
-  Change = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+  Input = event => {
+    this.setState({ todoInput: event.target.value });
   };
-
-  update = event => {
-    event.preventDefault();
-
+  Submit = () => {
     let todosCopy = this.state.todos.slice();
-    todosCopy.push({ task: this.state.task, id: Date.now(), completed: false });
+    todosCopy.push({
+      task: this.state.todoInput,
+      id: Date.now(),
+      completed: false
+    });
     this.setState({ todos: todosCopy, todoInput: "" });
+  };
+  Done = event => {
+    for (let i = 0; i < this.state.todos.length; i++) {
+      if (this.state.todos[i].id === event) {
+        let todoscopy = [...this.state.todos];
+        todoscopy[i].completed = !todoscopy[i].completed;
+        this.setState({ todos: todoscopy });
+      }
+    }
+    console.log(this.state.todos);
+  };
+  Remove = () => {
+    console.log(this.state.todos);
+    let filter = [...this.state.todos].filter(todos => !todos.completed);
+    this.setState({ todos: filter });
   };
 
   render() {
     return (
-      <div className="main">
-        <div>
-          {this.state.todos.map(todo => (
-            <Todo key={todo.id} todo={todo} />
-          ))}
-        </div>
-        <TodoForm
-          task={this.state.task}
-          Change={this.Change}
-          update={this.update}
-        />
+      <div>
+        {this.state.todos.map(todo => (
+          <div
+            className={todo.completed == true ? "line" : ""}
+            onClick={() => this.Done(todo.id)}
+          >
+            {todo.task}
+          </div>
+        ))}
+        <input value={this.state.todoInput} onChange={this.Input} />
+        <button onClick={this.Submit}>Add</button>
+        <button onClick={this.Remove}>Remove</button>
       </div>
     );
   }
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Todo(props) {
-  //   console.log({ props });
+  console.log({ props });
   return (
     <div>
-      <div className="list">
+      <div className="list" onClick={() => props.complete(props.todo.task)}>
         <h3>TASK:{props.todo.task}</h3>
       </div>
     </div>
